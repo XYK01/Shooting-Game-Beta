76395953a2eec0c85e8f493c68ab5a029eec263b
小凯 <kaiyue2442@gmail.com>
2025-06-09 17:43:31 +0200

    game

--- Assets/PlayerShooting.cs
+++ Assets/PlayerShooting.cs
@@ -6,15 +6,24 @@ public class PlayerShooting : MonoBehaviour
     public Camera cam;
     public float range = 100f;
     public LayerMask shootableLayers;
+
     public Button fireButton;
+    public GameObject muzzleFlashPrefab;
+    public Transform muzzlePoint;
 
     void Start()
     {
-        fireButton.onClick.AddListener(Shoot); // attach event
+        fireButton.onClick.AddListener(Shoot);
     }
 
     void Shoot()
     {
+        if (muzzleFlashPrefab && muzzlePoint)
+        {
+            GameObject flash = Instantiate(muzzleFlashPrefab, muzzlePoint.position, muzzlePoint.rotation);
+            Destroy(flash, 0.5f);
+        }
+
         Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
         if (Physics.Raycast(ray, out RaycastHit hit, range, shootableLayers))
         {
